d45ce3eb67f9318859d5e12b876baa4d42c05210
Pr0b1 <mateusz.rutkowski566@gmail.com>
2022-03-01 11:02:52 +0100

    Algorytm wyznaczania liczb pierwszych

---
+++ algorytm wyznaczania liczb pierwszych/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int liczba;
+    cout << "Podaj liczbę by sprawdzić czy jest liczbą pierwszą: " << endl;
+    cin >> liczba;
+    for (int i = 2; liczba%i==0; i++) {
+        int wynik = liczba % i;
+        if (wynik != 0) {
+            
+        }
+        if (wynik == 0) {
+            cout << liczba << " jest liczbą pierwszą!";
+            break;
+        }
+    }
+}
+
+
